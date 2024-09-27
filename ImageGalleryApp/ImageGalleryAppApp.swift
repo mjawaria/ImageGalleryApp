478ddd875cfa798679dcc60044dfddc6160a850a
Manendra Singh <43958993+mjawaria@users.noreply.github.com>
2024-09-27 19:55:48 +0530

    Initial Commit

---
+++ ImageGalleryApp/ImageGalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryAppApp.swift
+//  ImageGalleryApp
+//
+//  Created by Mukul on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
